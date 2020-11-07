9204b4678cba56d8404c16d719d8d0706b8c35de
lemonliu1993 <2305247091@qq.com>
2020-11-07 10:56:52 +0800

    java8-普通方法过滤

--- java8/src/main/java/com/lemon/java8/test/TestLambda.java
+++ java8/src/main/java/com/lemon/java8/test/TestLambda.java
@@ -2,8 +2,7 @@ package com.lemon.java8.test;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Comparator;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * Created by lemoon on 2020/11/6 8:12 AM
@@ -30,5 +29,42 @@ public class TestLambda {
         TreeSet<Integer> ts = new TreeSet<>(com);
     }
 
+    //需求：获取当前公司中员工年龄大于35的员工信息
+    @Test
+    public void test3() {
+        List<Employee> list = filterEmployees(employees);
+        for (Employee employee : list) {
+            System.out.println(employee);
+        }
+    }
+
+    List<Employee> employees = Arrays.asList(
+            new Employee("张三", 18, 9999.99),
+            new Employee("李四", 38, 5555.99),
+            new Employee("王五", 50, 6666.66),
+            new Employee("赵六", 16, 3333.33),
+            new Employee("田七", 8, 7777.77)
+    );
+
+    public List<Employee> filterEmployees(List<Employee> list) {
+        List<Employee> emps = new ArrayList<>();
+        for (Employee emp : list) {
+            if (emp.getAge() >= 35) {
+                emps.add(emp);
+            }
+        }
+        return emps;
+    }
+
+    //需求：获取员工工资大于5000的员工信息
+    public List<Employee> filterEmployees2(List<Employee> list) {
+        List<Employee> emps = new ArrayList<>();
+        for (Employee emp : list) {
+            if (emp.getSalary() >= 5000) {
+                emps.add(emp);
+            }
+        }
+        return emps;
+    }
 
 }
