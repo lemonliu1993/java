ae103a402b6ded1f25b74c1e5016ff02eb409e91
lemonliu1993 <2305247091@qq.com>
2023-03-12 23:13:50 +0800

    basic filter employees

---
+++ java8/src/main/java/com/lemon/java8/TestLambda.java
@@ -0,0 +1,58 @@
+package com.lemon.java8;
+
+import com.lemon.java8.entity.Employee;
+import org.junit.Test;
+
+import java.util.*;
+
+/**
+ * Created by lemoon on 2023/3/11 23:56
+ */
+public class TestLambda {
+    @Test
+    public void test1() {
+        Comparator<Integer> com = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return Integer.compare(o1, o2);
+            }
+        };
+
+        TreeSet<Integer> ts = new TreeSet<Integer>(com);
+    }
+
+    //Lambda 表达式
+    @Test
+    public void test2() {
+        Comparator<Integer> com = (x, y) -> Integer.compare(x, y);
+        TreeSet<Integer> ts = new TreeSet<>(com);
+    }
+
+    List<Employee> employees = Arrays.asList(
+            new Employee("张三", 20L, 10000.0),
+            new Employee("李四", 31L, 13000.0),
+            new Employee("王五", 42L, 15000.0),
+            new Employee("赵六", 53L, 17000.0),
+            new Employee("田七", 64L, 10000.0)
+    );
+
+    //需求：获取当前公司中员工年龄大于35的员工信息
+    @Test
+    public void test3() {
+        List<Employee> employees = filterEmployees(this.employees);
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
+    }
+
+    public List<Employee> filterEmployees(List<Employee> list) {
+        List<Employee> emps = new ArrayList<>();
+        for (Employee emp : list) {
+            if (emp.getAge() >= 35) {
+                emps.add(emp);
+            }
+        }
+        return emps;
+    }
+
+}
---
+++ java8/src/main/java/com/lemon/java8/entity/Employee.java
@@ -0,0 +1,49 @@
+package com.lemon.java8.entity;
+
+/**
+ * Created by lemoon on 2023/3/12 23:05
+ */
+public class Employee {
+    private String name;
+    private Long age;
+    private Double salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getAge() {
+        return age;
+    }
+
+    public void setAge(Long age) {
+        this.age = age;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Employee(String name, Long age, Double salary) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", salary=" + salary +
+                '}';
+    }
+}
