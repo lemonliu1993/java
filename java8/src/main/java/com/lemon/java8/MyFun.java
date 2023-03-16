51884bc20d26f7d6c73a0ee9b00e1a163b7dfab9
lemonliu1993 <2305247091@qq.com>
2023-03-16 23:30:19 +0800

    函数式接口

---
+++ java8/src/main/java/com/lemon/java8/MyFun.java
@@ -0,0 +1,12 @@
+package com.lemon.java8;
+
+/**
+ * Created by lemoon on 2023/3/16 23:29
+ */
+@FunctionalInterface
+public interface MyFun {
+
+    public Integer getValue(Integer num);
+
+//    public Integer getValue1(Integer num);
+}
